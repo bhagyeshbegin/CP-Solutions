ffddc6c59c7d6e445d32028b82ab3b568fc507e2
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-08-30 19:06:00 +0530

    Add Наиболее социально-дистанцированная подпоследовательность (1300)

---
+++ codeforces/1300/1364B - Наиболее социально-дистанцированная подпоследовательность.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+        vector<ll> ans;
+        ans.push_back(a[0]);
+        for(int i=1;i<n-1;i++){
+            if((a[i-1]<a[i])!=(a[i]<a[i+1])){
+                ans.push_back(a[i]);
+            }
+        }
+                ans.push_back(a[n-1]);
+        cout << ans.size() << endl;
+        for(auto it:ans){
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
