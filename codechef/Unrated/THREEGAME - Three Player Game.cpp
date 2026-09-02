8dd9861de53e9f771fa42567f5cdfcf7eaa97865
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-02 21:20:47 +0530

    Add Three Player Game (Unrated)

---
+++ codechef/Unrated/THREEGAME - Three Player Game.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n==1){
+            cout << 1 << endl;
+        }
+        else if(n%2==0){
+            cout << n+(n/2)+1 << endl;
+        }
+        else {
+            cout << n+n/2 << endl;
+        }
+    }
+}
