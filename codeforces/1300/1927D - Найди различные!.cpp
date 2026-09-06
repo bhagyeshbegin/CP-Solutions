c0340bcf941212a19478e47a12c3e979aef99e20
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-06 12:00:19 +0530

    Add Найди различные! (1300)

---
+++ codeforces/1300/1927D - Найди различные!.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// Constants
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+//---  Fast I/O MACROS -----
+#define fast_io \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Mathematics
+ll gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else {
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll mod_add(ll a,ll b,ll m = MOD){
+    return (a%m+b%m)%m;
+}
+
+ll mod_sub(ll a,ll b, ll m = MOD){
+    return (a%m - b% m+m)%m;
+}
+
+ll mod_multi(ll a,ll b,ll m = MOD){
+    return (a%m*b%m)%m;
+}
+
+const int N = 1000000;
+
+ll modpow(ll a,ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b & 1){
+            ans = ans*a % MOD;
+        }
+        a = a*a %  MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll fact[N+1];
+ll invfact[N+1];
+
+void precompute(){
+    //Factorial
+    fact[0] = 1;
+    for(int i=1;i<=N;i++){
+           fact[i] = fact[i-1]*i % MOD;
+    }
+    //inverse factorial
+    invfact[N] = modpow(fact[N],MOD-2);
+    for(int i=1;i<N;i++){
+        invfact[i-1] = invfact[i]*i % MOD;
+    }
+}
+
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+ 
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+ 
+        a = a * a;
+        b >>= 1;
+    }
+ 
+    return res;
+}
+
+
+// ----- SOLVE ------
+
+void solve(ll test){
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i=0;i<n;i++){
+         cin >> a[i];
+    }
+    ll prev = -1;
+    vector<ll> previndex(n);
+    for(int i=1;i<n;i++){
+        if(a[i]!=a[i-1]){
+            prev = i-1;
+        }
+        previndex[i] = prev; //storing last previous index;
+    }
+    ll q;
+    cin >> q;
+    while(q--){
+        ll l,r;
+        cin >> l >> r;
+        if(previndex[r-1]>=l-1){
+            cout << previndex[r-1]+1 << " " << r << endl;
+        }
+        else {
+            cout << -1 << " " << -1 << endl;
+        }
+    }
+}
+
+
+
+// ---- MAIN -----
+int main(){
+    fast_io
+    long long t = 1;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        solve(i);
+    }
+    return 0;
+}
