64a2691473f045e6de93cd1d75f20d0de78cf621
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-10 00:44:31 +0530

    Add Медвежонок и простота за 100 (1400)

---
+++ codeforces/1400/679A - Медвежонок и простота за 100.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// Constants
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+//---  Fast I/O MACROS -----
+#define fast_io \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Mathematics
+ll gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else {
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll mod_add(ll a,ll b,ll m = MOD){
+    return (a%m+b%m)%m;
+}
+
+ll mod_sub(ll a,ll b, ll m = MOD){
+    return (a%m - b% m+m)%m;
+}
+
+ll mod_multi(ll a,ll b,ll m = MOD){
+    return (a%m*b%m)%m;
+}
+
+const int N = 1000000;
+
+ll modpow(ll a,ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b & 1){
+            ans = ans*a % MOD;
+        }
+        a = a*a %  MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll fact[N+1];
+ll invfact[N+1];
+
+void precompute(){
+    //Factorial
+    fact[0] = 1;
+    for(int i=1;i<=N;i++){
+           fact[i] = fact[i-1]*i % MOD;
+    }
+    //inverse factorial
+    invfact[N] = modpow(fact[N],MOD-2);
+    for(int i=1;i<N;i++){
+        invfact[i-1] = invfact[i]*i % MOD;
+    }
+}
+
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+ 
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+ 
+        a = a * a;
+        b >>= 1;
+    }
+ 
+    return res;
+}
+
+
+// ----- SOLVE ------
+
+void solve(ll test){
+   ll count = 0;
+   string s;
+   int a[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,4,9,25,49};//in this we have stored all prime numbers and square 
+   for (int x:a){
+      cout << x << endl;
+      cin >> s;
+     if(s=="yes"){
+         count++;  // if there are 2 yes then it is composite if its 1 yes then will also check square and 0 then it will be prime.
+     }
+     if(count>=2){  //if we get two yes then it is composite
+         cout << "composite" << endl;
+         return;
+     }
+   }
+   cout << "prime" << endl;
+}
+
+
+
+
+// ---- MAIN -----
+int main(){
+    fast_io
+    long long t = 1;
+    for(int i=1;i<=t;i++){
+        solve(i);
+    }
+    return 0;
+}
