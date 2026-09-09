0bba961e8e2965801a482b6fd6328cc1ae97b684
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-09 15:44:22 +0530

    Add Контест Монокарпа (Unrated)

---
+++ codeforces/Unrated/2260A - Контест Монокарпа.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// Constants
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+//---  Fast I/O MACROS -----
+#define fast_io \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Mathematics
+ll gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else {
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll mod_add(ll a,ll b,ll m = MOD){
+    return (a%m+b%m)%m;
+}
+
+ll mod_sub(ll a,ll b, ll m = MOD){
+    return (a%m - b% m+m)%m;
+}
+
+ll mod_multi(ll a,ll b,ll m = MOD){
+    return (a%m*b%m)%m;
+}
+
+const int N = 1000000;
+
+ll modpow(ll a,ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b & 1){
+            ans = ans*a % MOD;
+        }
+        a = a*a %  MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll fact[N+1];
+ll invfact[N+1];
+
+void precompute(){
+    //Factorial
+    fact[0] = 1;
+    for(int i=1;i<=N;i++){
+           fact[i] = fact[i-1]*i % MOD;
+    }
+    //inverse factorial
+    invfact[N] = modpow(fact[N],MOD-2);
+    for(int i=1;i<N;i++){
+        invfact[i-1] = invfact[i]*i % MOD;
+    }
+}
+
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+ 
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+ 
+        a = a * a;
+        b >>= 1;
+    }
+ 
+    return res;
+}
+
+
+// ----- SOLVE ------
+
+void solve(ll test){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    int zeros = 0;
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            zeros++;
+        }
+    }
+    if(zeros<2){
+        cout << -1 << endl;
+    }
+    else if(a[0]== 0 && a[n-1]==0){
+        cout << 0 << endl;
+    }
+    else if((a[0]==0 && a[n-1]==1) || (a[0]==1 && a[n-1]==0)){
+        cout << 1 << endl;
+    }
+    else {
+        cout << 2 << endl;
+    }
+}
+
+
+
+
+// ---- MAIN -----
+int main(){
+    fast_io
+    long long t = 1;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        solve(i);
+    }
+    return 0;
+}
