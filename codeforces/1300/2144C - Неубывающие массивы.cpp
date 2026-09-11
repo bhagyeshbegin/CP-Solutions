148ecaa1f78ba1a4edf78935d4602b4b4c309209
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-11 23:00:24 +0530

    Add Неубывающие массивы (1300)

---
+++ codeforces/1300/2144C - Неубывающие массивы.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// Constants
+const int INF = 1e9;
+const int MOD = 998244353;
+
+//---  Fast I/O MACROS -----
+#define fast_io \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Mathematics
+ll gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else {
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll mod_add(ll a,ll b,ll m = MOD){
+    return (a%m+b%m)%m;
+}
+
+ll mod_sub(ll a,ll b, ll m = MOD){
+    return (a%m - b% m+m)%m;
+}
+
+ll mod_multi(ll a,ll b,ll m = MOD){
+    return (a%m*b%m)%m;
+}
+
+const int N = 1000000;
+
+ll modpow(ll a,ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b & 1){
+            ans = ans*a % MOD;
+        }
+        a = a*a %  MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll fact[N+1];
+ll invfact[N+1];
+
+void precompute(){
+    //Factorial
+    fact[0] = 1;
+    for(int i=1;i<=N;i++){
+           fact[i] = fact[i-1]*i % MOD;
+    }
+    //inverse factorial
+    invfact[N] = modpow(fact[N],MOD-2);
+    for(int i=1;i<N;i++){
+        invfact[i-1] = invfact[i]*i % MOD;
+    }
+}
+
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+ 
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+ 
+        a = a * a;
+        b >>= 1;
+    }
+ 
+    return res;
+}
+
+
+// ----- SOLVE ------
+
+void solve(ll test){
+   ll n;
+   cin >> n;
+   vector<ll> a(n);
+   for(int i=0;i<n;i++){
+    cin >> a[i];
+   }
+   vector<ll> b(n);
+   for(int i=0;i<n;i++){
+    cin >> b[i];
+   }
+   ll ans = 1;
+   for(int i=0;i<n;i++){
+    if(a[i]>b[i]){
+        swap(a[i],b[i]);
+    }
+    if(i==0 || a[i]>=b[i-1]){ //a[i]>=b[i-1]>=a[i-1]....
+       ans  = ans *2 %MOD ; // just observation
+    }
+   }
+   cout << (ans) << endl;
+}
+
+
+
+
+// ---- MAIN -----
+int main(){
+    fast_io
+    long long t = 1;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        solve(i);
+    }
+    return 0;
+}
