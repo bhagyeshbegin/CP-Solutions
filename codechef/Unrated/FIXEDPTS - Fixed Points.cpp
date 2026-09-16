02ebe66d0fe6e5a7e6be6042446e84a1b6c98605
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-16 20:18:18 +0530

    Add Fixed Points (Unrated)

---
+++ codechef/Unrated/FIXEDPTS - Fixed Points.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        int count = 0;
+        if((n-k)>1 || (n-k)==0){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+}
