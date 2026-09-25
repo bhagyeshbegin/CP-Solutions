ac2b229f1305eb9693616d4584c12771b9099ebc
Bhagyesh Patil <bhagyeshp989@gmail.com>
2026-09-25 11:07:08 +0530

    Add Ферма пилюль Найфа (Unrated)

---
+++ codeforces/Unrated/2264B - Ферма пилюль Найфа.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// Constants
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+//---  Fast I/O MACROS -----
+#define fast_io \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// Mathematics
+ll gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else {
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+
+ll mod_add(ll a,ll b,ll m = MOD){
+    return (a%m+b%m)%m;
+}
+
+ll mod_sub(ll a,ll b, ll m = MOD){
+    return (a%m - b% m+m)%m;
+}
+
+ll mod_multi(ll a,ll b,ll m = MOD){
+    return (a%m*b%m)%m;
+}
+
+const int N = 1000000;
+
+ll modpow(ll a,ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b & 1){
+            ans = ans*a % MOD;
+        }
+        a = a*a %  MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+ll fact[N+1];
+ll invfact[N+1];
+
+void precompute(){
+    //Factorial
+    fact[0] = 1;
+    for(int i=1;i<=N;i++){
+           fact[i] = fact[i-1]*i % MOD;
+    }
+    //inverse factorial
+    invfact[N] = modpow(fact[N],MOD-2);
+    for(int i=1;i<N;i++){
+        invfact[i-1] = invfact[i]*i % MOD;
+    }
+}
+
+long long binpow(long long a, long long b)
+{
+    long long res = 1;
+ 
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+ 
+        a = a * a;
+        b >>= 1;
+    }
+ 
+    return res;
+}
+
+
+// ----- SOLVE ------
+
+void solve(ll test){
+   ll n,m;
+   cin >> n >> m;
+   vector<ll> a(n);
+   for(int i=0;i<n;i++){
+    cin >> a[i];
+   }
+   ll ans = LLONG_MIN;
+   priority_queue<ll> pq;
+   ll sum = 0;
+   for(int i=0;i<n;i++){
+      if(pq.size()==m-1){
+        ans = max(ans,m*a[i]-sum);
+      }
+      //pq in this works like max heap so its root element stores the larget element.
+      pq.push(a[i]);
+      sum += a[i];
+      if(pq.size()>m-1){ //As we want only m-1 smallest element. if one extra element comes will reduce it that will be the largest element.
+        sum -= pq.top();   //we want to construct sum of smallest m-1 elements according to formula derived.
+        pq.pop();
+      }
+   }
+   cout << ans << endl;
+}
+
+
+
+
+// ---- MAIN -----
+int main(){
+    fast_io
+    long long t = 1;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        solve(i);
+    }
+    return 0;
+}
